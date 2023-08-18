9ced15b729c2959d33b2f606d30e7dd3a65c62e5
kennavaughn <125404329+kennavaughn@users.noreply.github.com>
2023-08-18 02:50:12 +0000

    Assignment-6-9:50

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,5 @@
 import "./styling.css";
+const Banner = 
 function Banner(){
     <><header>Orbit Report</header>
     <p>Click on the buttons to see the satellites in that orbit type</p>
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,15 +1,14 @@
 import "./styling.css";
 import satData from "./satData"
 const Buttons = () => {
-const displaySats = ["Communication", "Positioning", "Probe", "Space Station", "Telescope", "Probe", "Space Debris","Communication", "Positioning", "Space Station",];
+const displaySats = ["Communication", "Positioning", "Probe", "Space Station", "Telescope", "Probe", "Space Debris","Communication", "Positioning", "Space Station"];
+const filterByType = ["Low", "Medium", "High"];
 const handleClick = () => {
-  console.log("Button clicked!");
 }
 displaySats.map((sat, id) => {
     return (
       <div className="flex-container">
-      <button onClick={() => filterByType(sat)} key={id}>
-        {sat} Orbit
+      <button onClick={() => filterByType(sat)} key={id}>{sat} Orbit
       <button onClick={handleClick}>Load Sat</button>
       <button id="filterButton" class="filter-btn">filterByType</button>
       <button>setSat</button>
